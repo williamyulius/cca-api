ac05dd886f72f42e8a88d92b650fee8753469c9b
williamyulius <williamyulius28@gmail.com>
2025-08-05 16:14:37 +0700

    Update seeder

--- database/seeders/MsStudentSeeder.php
+++ database/seeders/MsStudentSeeder.php
@@ -15,19 +15,177 @@ class MsStudentSeeder extends Seeder
     {
 
         $faker = \Faker\Factory::create('id_ID');
-        $characters = 'ABCDEF';
-        for($i=1;$i<20;$i++){
-            $randomNumbers = mt_rand(1,6);
-            $randomLetter = $characters[mt_rand(0,strlen($characters) - 1)];
-            $studentClass = $randomNumbers.$randomLetter;
-            MsStudent::create([
-                'student_id'=>sprintf('%06d',$i),
-                'name'=>$faker->name,
-                'unit'=>'SD',
-                'class'=>$studentClass,
-                'gender'=>$faker->randomElement(['L','P']),
-                'religion'=>'Katolik',
-            ]);
+        // $characters = 'ABCDEF';
+        $idCounter = 1;
+        for($i=1;$i<=6;$i++){
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'A',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'B',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'C',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'D',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'E',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
+            for($ctr=1;$ctr<=3;$ctr++){
+                MsStudent::create([
+                    'student_id'=>sprintf('%06d',$idCounter),
+                    'name'=>$faker->name,
+                    'unit'=>'SD',
+                    'class'=>($i).'F',
+                    'gender'=>$faker->randomElement(['L','P']),
+                    'religion'=>'Katolik',
+                ]);
+                $idCounter++;
+            }
         }
+
+        // for($i=1;$i<20;$i++){
+        //     if($i>=1){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'A',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     } elseif($i>3){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'B',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     } elseif($i>7){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'C',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     } elseif($i>10){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'D',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     } elseif($i>14){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'E',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     } elseif($i>17){
+        //         MsStudent::create([
+        //             'student_id'=>sprintf('%06d',$i),
+        //             'name'=>$faker->name,
+        //             'unit'=>'SD',
+        //             'class'=>($i%7).'F',
+        //             'gender'=>$faker->randomElement(['L','P']),
+        //             'religion'=>'Katolik',
+        //         ]);
+        //     }
+        //     // $randomNumbers = mt_rand(1,6);
+        //     // $randomLetter = $characters[mt_rand(0,strlen($characters) - 1)];
+        //     // $studentClass = $randomNumbers.$randomLetter;
+        //     // MsStudent::create([
+        //     //     'student_id'=>sprintf('%06d',$i),
+        //     //     'name'=>$faker->name,
+        //     //     'unit'=>'SD',
+        //     //     'class'=>$i%7.,
+        //     //     'gender'=>$faker->randomElement(['L','P']),
+        //     //     'religion'=>'Katolik',
+        //     // ]);
+        // }
+
+        // MsStudent::create([
+        //     'student_id'=>sprintf('%06d',$i),
+        //     'name'=>$faker->name,
+        //     'unit'=>'SD',
+        //     'class'=>$studentClass,
+        //     'gender'=>$faker->randomElement(['L','P']),
+        //     'religion'=>'Katolik',
+        // ]);
+        // MsStudent::create([
+        //     'student_id'=>sprintf('%06d',$i),
+        //     'name'=>$faker->name,
+        //     'unit'=>'SD',
+        //     'class'=>$studentClass,
+        //     'gender'=>$faker->randomElement(['L','P']),
+        //     'religion'=>'Katolik',
+        // ]);
+        // MsStudent::create([
+        //     'student_id'=>sprintf('%06d',$i),
+        //     'name'=>$faker->name,
+        //     'unit'=>'SD',
+        //     'class'=>$studentClass,
+        //     'gender'=>$faker->randomElement(['L','P']),
+        //     'religion'=>'Katolik',
+        // ]);
+        // MsStudent::create([
+        //     'student_id'=>sprintf('%06d',$i),
+        //     'name'=>$faker->name,
+        //     'unit'=>'SD',
+        //     'class'=>$studentClass,
+        //     'gender'=>$faker->randomElement(['L','P']),
+        //     'religion'=>'Katolik',
+        // ]);
     }
 }
--- database/seeders/TrStudentCoCurricularActivitySeeder.php
+++ database/seeders/TrStudentCoCurricularActivitySeeder.php
@@ -15,39 +15,9 @@ class TrStudentCoCurricularActivitySeeder extends Seeder
     {
         TrStudentCoCurricularActivity::create([
             'ms_subject_co_curricular_activity_id'=>'13',
-            'ms_student_id'=>'1',
-            'ms_class_id'=>'1',
-            'ms_group_id'=>'1',
-            'semester'=>'1',
-            'academic_year'=>'2025/2026',
-            'unit'=>'SD',
-            'campus'=>'LAS',
-        ]);
-        TrStudentCoCurricularActivity::create([
-            'ms_subject_co_curricular_activity_id'=>'4',
-            'ms_student_id'=>'3',
-            'ms_class_id'=>'18',
-            'ms_group_id'=>'3',
-            'semester'=>'1',
-            'academic_year'=>'2025/2026',
-            'unit'=>'SD',
-            'campus'=>'LAS',
-        ]);
-        TrStudentCoCurricularActivity::create([
-            'ms_subject_co_curricular_activity_id'=>'6',
-            'ms_student_id'=>'6',
-            'ms_class_id'=>'19',
-            'ms_group_id'=>'4',
-            'semester'=>'1',
-            'academic_year'=>'2025/2026',
-            'unit'=>'SD',
-            'campus'=>'LAS',
-        ]);
-        TrStudentCoCurricularActivity::create([
-            'ms_subject_co_curricular_activity_id'=>'2',
-            'ms_student_id'=>'2',
-            'ms_class_id'=>'36',
-            'ms_group_id'=>'6',
+            'ms_student_id'=>'19',
+            'ms_class_id'=>'7',
+            'ms_group_id'=>'2',
             'semester'=>'1',
             'academic_year'=>'2025/2026',
             'unit'=>'SD',
@@ -55,8 +25,48 @@ class TrStudentCoCurricularActivitySeeder extends Seeder
         ]);
         TrStudentCoCurricularActivity::create([
             'ms_subject_co_curricular_activity_id'=>'13',
-            'ms_student_id'=>'4',
-            'ms_class_id'=>'17',
+            'ms_student_id'=>'20',
+            'ms_class_id'=>'7',
+            'ms_group_id'=>'2',
+            'semester'=>'1',
+            'academic_year'=>'2025/2026',
+            'unit'=>'SD',
+            'campus'=>'LAS',
+        ]);
+        TrStudentCoCurricularActivity::create([
+            'ms_subject_co_curricular_activity_id'=>'13',
+            'ms_student_id'=>'21',
+            'ms_class_id'=>'7',
+            'ms_group_id'=>'2',
+            'semester'=>'1',
+            'academic_year'=>'2025/2026',
+            'unit'=>'SD',
+            'campus'=>'LAS',
+        ]);
+        TrStudentCoCurricularActivity::create([
+            'ms_subject_co_curricular_activity_id'=>'13',
+            'ms_student_id'=>'22',
+            'ms_class_id'=>'8',
+            'ms_group_id'=>'2',
+            'semester'=>'1',
+            'academic_year'=>'2025/2026',
+            'unit'=>'SD',
+            'campus'=>'LAS',
+        ]);
+        TrStudentCoCurricularActivity::create([
+            'ms_subject_co_curricular_activity_id'=>'10',
+            'ms_student_id'=>'28',
+            'ms_class_id'=>'10',
+            'ms_group_id'=>'2',
+            'semester'=>'1',
+            'academic_year'=>'2025/2026',
+            'unit'=>'SD',
+            'campus'=>'LAS',
+        ]);
+        TrStudentCoCurricularActivity::create([
+            'ms_subject_co_curricular_activity_id'=>'14',
+            'ms_student_id'=>'37',
+            'ms_class_id'=>'13',
             'ms_group_id'=>'3',
             'semester'=>'1',
             'academic_year'=>'2025/2026',
